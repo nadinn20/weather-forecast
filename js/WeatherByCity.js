8a5ff73e6fee832a542c86fa141d2e3e97158ed5
nadinn20 <nadja.shpytkovska@gmail.com>
2015-12-17 16:22:08 +0000

    changes

--- js/WeatherByCity.js
+++ js/WeatherByCity.js
@@ -1,7 +1,7 @@
 function getWeatherByCity(lang, fnOK, fnError, cityName) {
     $.getJSON(
         'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
-        + cityName + '&APPID=e522dd5a533738e9c18cde03828c57b1cnt=16&units=metric' + '&lang=' + lang + '&callback=?',
+        + cityName + '&APPID=e522dd5a533738e9c18cde03828c57b1&cnt=16&units=metric' + '&lang=' + lang + '&callback=?',
         function (data) {
             fnOK.call(this, data);
         }
--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
-    
+ function dataReceived(data) {
+		// Get the offset from UTC (turn the offset minutes into ms)
+		var offset = (new Date()).getTimezoneOffset()*60*1000;
+		$.getJSON(localTime), function(response){
+		    console.log(response)
+			$('#localTime').html(response.newDate(this.dt*1000 - offset));
+		};
+}
+
  $(function() {
-    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=istanbul'+
-    '&units=metric&cnt=7&APPID=e522dd5a533738e9c18cde03828c57b1',
-    function(data) {
-        data.list[0].temp.day;
-        $('#tempToday').html(data.list[0].temp.day);
-        $('#tempTomorrow').html(data.list[1].temp.day);
-        $('#tempAfterTommorow').html(data.list[2].temp.day);
-        $('#pressureToday').html(data.list[0].pressure);
-        $('#pressureTomorrow').html(data.list[1].pressure);
-        $('#pressureAfterTommorow').html(data.list[2].pressure);
-    }
-  );
+navigator.geolocation.getCurrentPosition(locSuccess, locError);
 });
+
+   function locSuccess(position) {
+   var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+
+                    '&lon='+position.coords.longitude+'&units=metric&APPID=e522dd5a533738e9c18cde03828c57b1&callback=?';
+           $.getJSON(weatherAPI, function(response) {
+               console.log(response);
+               $('#location').html(response.name);
+
+               $('#tempToday').html(Math.round(response.main.temp));
+               $('#pressureToday').html(response.main.pressure);
+               $('#humidityToday').html(response.main.humidity);
+               $('#icon1').html('<img src="images/'+ icon + '.png" alt="Weather icon">');
+               
+               $('#tempTomorrow').html(Math.round(response.temp));
+               $('#pressureTommorow').html(response.pressure);
+               $('#humidityTomorrow').html(response.humidity);
+               
+               $('#tempAfterTomorrow').html(Math.round(response.temp));
+               $('#pressureAfterTommorow').html(response.pressure);
+               $('#humidityAfterTommorow').html(response.humidity);
+               
+               
+           });
+        }
+
+ function locError(error) {
+           console.warn('Error:' + error.message);
+}
