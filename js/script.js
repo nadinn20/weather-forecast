184d938ca2d5ed556fc1f26e209aa8bd2794063e
nadinn20 <nadja.shpytkovska@gmail.com>
2015-12-21 22:44:51 +0000

    deleted files html and js

--- js/script.js
+++ js/script.js
@@ -1,72 +1,118 @@
-$(function(){
-    moment.locale('uk');
-    $('#btnGetWeather').click(function () {
-        getWeatherByCity('ua', insertWeatherData, locError, $('#inputCityName').val());
-    });
-    // Adding handler for 'Enter' key on keyboard
-    $('#inputCityName').keypress(function(e) {
-        var ENTER_KEY_CODE = 13;
-        if ( e.which === ENTER_KEY_CODE ) 
-        {
-            $('#btnGetWeather').trigger('click');
-            return false;
-        }
-    });  
-$(function getWeatherData(fnOK, fnError) {
-navigator.geolocation.getCurrentPosition(locSuccess,locError);
-});
+function Weather() {
 
-   function locSuccess(position) {
-   var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+
-                    '&lon='+position.coords.longitude+'&units=metric&APPID=e522dd5a533738e9c18cde03828c57b1&callback=?';
-   var offset = (new Date()).getTimezoneOffset()*60*1000; 
-            
-           $.getJSON(weatherAPI, function(response) {
-               console.log(response);
-               $('#location').html(response.name);
-               $('#localTime').html(moment(new Date(response.dt*1000-offset)).calendar());
-               $('#temp').html(Math.round(response.main.temp));
-               $('#temp_min').html(Math.round(response.main.temp_min));
-               $('#temp_max').html(Math.round(response.main.temp_max));
-               $('#pressure').html(Math.round(response.main.pressure*0.75006375541921));
-               $('#humidity').html(response.main.humidity);
-               $('#windSpeed').html(response.wind.speed);
-               $('#clouds').html(response.clouds.all);
-               $('#icon').html('<img src="/images/icons'+ icon +'.png" alt="' + weather.description + '" >');
-               
-           });
-        }
+    this.setUpGeolocation = function() {
+        navigator.geolocation.getCurrentPosition(onLocationSuccess, onLocationError);
+    };
 
- 
-function insertWeatherData(data){
-
-        $("#weatherTable tr:not(:first)").remove();
-
-        $.each(data.list, function(){
-            // "this" тримає об'єкт прогнозу звідси: http://openweathermap.org/forecast16
-            var localTime = new Date(this.dt*1000 - offset); // конвертуємо час з UTC у локальний
-            addWeather(
-                this.weather[0].icon,
-                moment(localTime).calendar(),	// Використовуємо moment.js для представлення дати
-                this.weather[0].description,
-                Math.round(this.temp.day) + '&deg;C'
-            );
+    this.startSearch = function() {
+        $('#btnGetWeather').click(function () {
+            var cityName =  $('#inputCityName').val();
+            if (cityName) {
+                getWeatherByCity(cityName);
+            }
         });
-        $('#location').html(city + ', <b>' + country + '</b>'); // Додаємо локацію на сторінку
+        $('#inputCityName').keypress(function(e) {
+            var ENTER_KEY_CODE = 13;
+            if ( e.which === ENTER_KEY_CODE ) {
+                $('#btnGetWeather').trigger('click');
+                return false;
+            }
+        });
+    };
 
-    function addWeather(icon, day, condition, temp){
-        var markup = '<tr>'+
-                '<td>' + day + '</td>' +
-                '<td>' + '<img src="images/icons/'+ 
-                  icon
-                  +'.png" />' + '</td>' +
-                '<td>' + temp + '</td>' +
-                '<td>' + condition + '</td>'
-            + '</tr>';
-        weatherTable.insertRow(-1).innerHTML = markup; // Додаємо рядок до таблиці
+    function onLocationSuccess(position) {
+        var url = getWeatherByPosUrl(position);
+        $.getJSON(url, onWeatherData);
+
+        var forecastUrl = getForecastByPosUrl(position);
+        $.getJSON(forecastUrl, populateForecast);
     }
+
+    function onLocationError(error) {
+        console.warn('Error:' + error.message);
+    }
+
+    function getWeatherByCity(cityName) {
+        var url = getWeatherByCityUrl(cityName);
+        $.getJSON(url, onWeatherData);
+
+        var forecastUrl = getForecastByCityUrl(position);
+        $.getJSON(forecastUrl, populateForecast);
+    }
+     // ---- Weather and Forecast by Location --- /
+    function getWeatherByPosUrl(position) {
+        return 'http://api.openweathermap.org/data/2.5/weather?lat='+position.coords.latitude+
+            '&lon='+position.coords.longitude+'&units=metric&APPID=e522dd5a533738e9c18cde03828c57b1&callback=?';
+    }
+
+    function getForecastByPosUrl(position) {
+        return 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+position.coords.latitude+
+            '&lon='+position.coords.longitude+'&cnt=7&APPID=e522dd5a533738e9c18cde03828c57b1&callback=?';
+    }
+    //---------------------------------------------/
+
+     // Weather and Forecast by City ---------------/
+    function getWeatherByCityUrl(cityName) {
+        return 'http://api.openweathermap.org/data/2.5/weather?q='+ cityName +
+            '&type=like&APPID=e522dd5a533738e9c18cde03828c57b1&callback=?';
+    }
+
+    function getForecastByCityUrl(cityName) {
+         return 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+ cityName +
+             '&units=metric&cnt=3&APPID=e522dd5a533738e9c18cde03828c57b1&callback=?';
+    }
+    //------------------------------------------/
+
+    function onWeatherData(data) {
+        var offset = (new Date()).getTimezoneOffset()*60*1000;
+        var updatedDate = new Date(data.dt*1000-offset);
+        var now = moment(updatedDate).calendar();
+        $('#location').html(data.name);
+        $('#localTime').html(now);
+        $('#temp').html(Math.round(data.main.temp));
+        $('#temp_min').html(Math.round(data.main.temp_min));
+        $('#temp_max').html(Math.round(data.main.temp_max));
+        $('#pressure').html(Math.round(data.main.pressure*0.75006375541921));
+        $('#humidity').html(data.main.humidity);
+        $('#windSpeed').html(data.wind.speed);
+        $('#clouds').html(data.clouds.all);
+        $('#icon').html('<img src="images/icons/'+ data.weather[0].icon +'.png" alt="' + data.weather[0].icon + ' ' + data.weather[0].description + '" >');
+    }
+
+    function populateForecast(data) {
+        var forecastList = data.list;
+        var offset = (new Date()).getTimezoneOffset()*60*1000;
+        
+        forecastList.forEach(function(forecast) {
+        $('localTime1').html(moment(new Date(data.list[0].dt*1000-offset)).calendar());
+        $('#temp1').html(Math.round(data.list[0].temp.day));
+        $('#pressure1').html(Math.round(data.list[0].pressure*0.75006375541921));
+        
+        $('localTime2').html(moment(new Date(data.list[1].dt*1000-offset)).calendar());
+        $('#temp2').html(Math.round(data.list[1].temp.day));
+        $('#pressure2').html(Math.round(data.list[1].pressure*0.75006375541921));
+        
+        $('localTime3').html(moment(new Date(data.list[2].dt*1000-offset)).calendar());
+        $('#temp3').html(Math.round(data.list[2].temp.day));
+        $('#pressure3').html(Math.round(data.list[2].pressure*0.75006375541921));
+        
+        $('localTime4').html(moment(new Date(data.list[3].dt*1000-offset)).calendar());
+        $('#temp4').html(Math.round(data.list[3].temp.day));
+        $('#pressure4').html(Math.round(data.list[3].pressure*0.75006375541921));
+        
+        $('localTime5').html(moment(new Date(data.list[4].dt*1000-offset)).calendar());
+        $('#temp5').html(Math.round(data.list[4].temp.day));
+        $('#pressure5').html(Math.round(data.list[4].pressure*0.75006375541921));
+        
+        $('localTime6').html(moment(new Date(data.list[5].dt*1000-offset)).calendar());
+        $('#temp6').html(Math.round(data.list[5].temp.day));
+        $('#pressure6').html(Math.round(data.list[5].pressure*0.75006375541921));
+        
+        $('localTime7').html(moment(new Date(data.list[6].dt*1000-offset)).calendar());
+        $('#temp7').html(Math.round(data.list[6].temp.day));
+        $('#pressure7').html(Math.round(data.list[6].pressure*0.75006375541921));
+        });
+    }
+
+
 }
-function locError(error) {
-    console.warn('Error:' + error.message);
-}
-});
